0df5af657825961342e1ab27b19d612265472f61
yongnology <cassius20@naver.com>
2024-05-19 23:48:23 +0900

    5.4 스프링 데이터와 스프링 데이터 JPA

--- springboot-developer/src/main/java/me/yongmin/springbootdeveloper/MemberRepository.java
+++ springboot-developer/src/main/java/me/yongmin/springbootdeveloper/MemberRepository.java
@@ -3,7 +3,11 @@ package me.yongmin.springbootdeveloper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.nio.ByteBuffer;
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByName(String name);
 }
---
+++ springboot-developer/src/test/java/me/yongmin/springbootdeveloper/MemberRepositoryTest.java
@@ -0,0 +1,46 @@
+package me.yongmin.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getAllMembers() {
+        // when
+        List<Member> members = memberRepository.findAll();
+
+        // then
+        assertThat(members.size()).isEqualTo(3);
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getMemberById() {
+        // when
+        Member member = memberRepository.findById(2L).get();
+
+        // then
+        assertThat(member.getName()).isEqualTo("B");
+    }
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getMemberByName() {
+        // when
+        Member member = memberRepository.findByName("C").get();
+
+        // then
+        assertThat(member.getId()).isEqualTo(3);
+    }
+}
