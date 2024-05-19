c77c3eae2236363001fb2d39a9dfe02c84465c1f
yongnology <cassius20@naver.com>
2024-05-19 23:52:24 +0900

    5.5 예제 코드 살펴보기

--- springboot-developer/src/main/java/me/yongmin/springbootdeveloper/Member.java
+++ springboot-developer/src/main/java/me/yongmin/springbootdeveloper/Member.java
@@ -6,16 +6,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 기본 생성자
 @AllArgsConstructor
 @Getter
 @Entity
 public class Member {
-    @Id
+    @Id // id 필드를 기본키로 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id", updatable = false)
     private Long id;    // DB 테이블의 'id' 컬럼과 매칭
     
     @Column(name="name", nullable = false)
     private String name;    // DB 테이블의 'name' 컬럼과 매칭
+
+    public void changeName(String name) {
+        this.name = name;
+    }
 }
